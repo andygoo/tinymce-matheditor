52f335b15f3876c7a4edc1a16d6ef34ef256ae3f
camazorro <gerardo-renovales@MACMINIDEV1.local>
2015-06-26 09:05:32 -0400

    TinyMCE MathEditor
    Initial Commit

---
+++ editor_plugin.js
@@ -0,0 +1,23 @@
+﻿(function (tinymce) {
+    var Icon = tinymce.baseURL + '/plugins3x/matheditor/icon/square_root.png';
+
+    tinymce.create('tinymce.plugins.matheditor', {
+        init: function (ed, url) {
+            // Register commands
+            ed.addCommand('mceMathEditor', function () {
+                ed.windowManager.open({
+                    file: url + '/html/plugin.html',
+                    width: 640,
+                    height: 420,
+                    inline: true
+                }, {
+                    plugin_url: url
+                });
+            });
+
+            ed.addButton('matheditor', { cmd: 'mceMathEditor', image: Icon, title: 'Math Editor', tooltip: 'Math Editor' });
+        },
+    });
+
+    tinymce.PluginManager.add('matheditor', tinymce.plugins.matheditor);
+})(tinymce);
